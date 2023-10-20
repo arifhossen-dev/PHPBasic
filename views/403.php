d55dd4dde72cdb8223898277188a0c752b4d598e
arifhossen-dev <ahak.bsl@gmail.com>
2023-10-20 14:04:45 +0600

    forbidden response page created

---
+++ views/403.php
@@ -0,0 +1,11 @@
+<?php require('partials/header.php') ?>
+<?php require('partials/nav.php') ?>
+
+<main>
+  <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+    You are not authorize in this task.
+    <a class="underline block text-blue-500" href="/notes">Back to home</a>
+  </div>
+</main>
+
+<?php require('partials/footer.php') ?>
