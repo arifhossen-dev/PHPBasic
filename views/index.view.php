edd4be6f0c5379807bfca83945420bbc6a8b8a47
arifhossen-dev <ahak.bsl@gmail.com>
2023-10-22 11:52:49 +0600

    all refactor with namespace

--- views/index.view.php
+++ views/index.view.php
@@ -1,11 +1,11 @@
-<?php require('partials/header.php') ?>
+<?php require('partials/head.php') ?>
 <?php require('partials/nav.php') ?>
 <?php require('partials/banner.php') ?>
 
 <main>
-  <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-    Home ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, qui. Porro error quae repellendus impedit possimus assumenda quibusdam, accusamus facilis iste id autem quos, laudantium ratione provident in sequi animi!
-  </div>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <p>Hello. Welcome to the home page.</p>
+    </div>
 </main>
 
 <?php require('partials/footer.php') ?>
