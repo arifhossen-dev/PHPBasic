88f0aebbcfbfee91a520c3c0b2f110248e47e7c3
arifhossen-dev <ahak.bsl@gmail.com>
2023-10-26 09:38:38 +0600

    classed base router create

--- Core/router.php
+++ Core/router.php
@@ -1,22 +1,50 @@
 <?php
+namespace Core;
+class Router 
+{
+    protected $routes = [];
 
- function routeToController($uri, $routes) {
-    if (array_key_exists($uri, $routes)) {
-        require base_path($routes[$uri]);
-    } else {
-        abort();
+    public function addRoute($method, $uri, $controller)
+    {
+        $this->routes[] = compact('method','uri','controller');
+    }
+
+    public function get($uri, $controller)
+    {
+        return $this->addRoute("GET", $uri, $controller);
+    }
+    public function post($uri, $controller)
+    {
+        return $this->addRoute("POST", $uri, $controller);
+    }
+    public function delete($uri, $controller)
+    {
+        return $this->addRoute("DELETE", $uri, $controller);
+    }
+    public function put($uri, $controller)
+    {
+        return $this->addRoute("PUT", $uri, $controller);
+    }
+    public function patch($uri, $controller)
+    {
+        return $this->addRoute("PATCH", $uri, $controller);
+    }
+
+    function route($uri, $method) {
+        foreach ($this->routes as $route) {
+            if ($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
+                return require base_path($route['controller']);
+            }
+        }
+
+        $this->abort();
+    }
+    
+    function abort($code = 404) {
+        http_response_code($code);
+    
+        require base_path("views/{$code}.php");
+    
+        die();
     }
 }
-
-function abort($code = 404) {
-    http_response_code($code);
-
-    require base_path("views/{$code}.php");
-
-    die();
-}
-
-$routes = require base_path('routes.php');
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-routeToController($uri, $routes);
